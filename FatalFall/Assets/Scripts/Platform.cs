c0c245e877ee8d233c0f59693a420ee3b6b79544
Daniel Valoria <djvaloria@cox.net>
2019-11-27 16:31:35 -0800

    Added color material, platforms, basic player movement, basic camera follow

---
+++ FatalFall/Assets/Scripts/Platform.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(gameObject + "\n");
+        Debug.Log("Success\n");
+        Debug.Log(collision.gameObject);
+    }
+}
