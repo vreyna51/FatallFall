6855d3b475591c132c833d5e72d80af321528e69
Daniel Valoria <djvaloria@cox.net>
2019-11-27 22:35:03 -0800

    Added camera to follow player and player to move direction mouse points

--- FatalFall/Assets/Scripts/Camera.cs
+++ FatalFall/Assets/Scripts/Camera.cs
@@ -6,19 +6,43 @@ public class Camera : MonoBehaviour
 {
     public Transform target;
     private Vector3 offset;
-
-    [Range(0.01f, 1.0f)]
-    public float smoothSpeed = 0.5f;
+    public float rotateSpeed;
+    public Transform pivot;
 
     // Start is called before the first frame update
     void Start()
     {
-        //target = GameObject.Find("Sphere").transform;
-        //offset = tranform.position - target.position;
+        offset = target.position - transform.position;
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+
+        //Cursor.lockState = CursorLockMode.Locked;
     }
+
     void LateUpdate()
     {
-        //Vector3 newPos = target.position + offset;
-        transform.position = Vector3.Slerp(transform.position, target.position, smoothSpeed);
+
+        //Get x position of mouse and rotate target
+        float horizontal = Input.GetAxis("Mouse X")*rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+
+        //Get y position of mouse and rotate pivot
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        pivot.Rotate(-vertical, 0, 0);
+
+        //show the camera based on current rotation of target & original offset
+        float desiredAngle = target.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+        //transform.position = target.position - offset;
+
+        if(transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y-0.5f, transform.position.z);
+        }
+
+        transform.LookAt(target);
     }
 }
